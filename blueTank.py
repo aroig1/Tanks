67a62cd1bd91f95de301c5c50a81992539f642cf
aroig1 <aroig@arizona.edu>
2023-11-25 22:58:27 -0700

    Moving tank with semi-working turret

---
+++ blueTank.py
@@ -0,0 +1,54 @@
+import pygame
+import math
+
+class BlueTank:
+    def __init__(self):
+
+        self.sprites = [pygame.image.load('SpriteImages/BlueTank/BlueTank1.bmp'), pygame.image.load('SpriteImages/BlueTank/BlueTank2.bmp'),
+                   pygame.image.load('SpriteImages/BlueTank/BlueTank3.bmp'), pygame.image.load('SpriteImages/BlueTank/BlueTank4.bmp')]
+        self.image = self.sprites[0]
+        self.x = 600
+        self.y = 400
+
+        self.turret = pygame.image.load('SpriteImages/BlueTank/BlueTurret.bmp')
+        self.turretX = self.x + (self.image.get_width() / 2) - (self.turret.get_width() / 2)
+        self.turretY = self.y + (self.image.get_height() / 2) - 80
+
+
+    def move(self, x, y):
+        self.x += x
+        self.y += y
+        self.turretX = self.x + (self.image.get_width() / 2) - (self.turret.get_width() / 2)
+        self.turretY = self.y + (self.image.get_height() / 2) - 80
+
+    def getTurret(self):
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+
+        width = mouse_x - (self.x + (self.image.get_width() / 2))
+        height = mouse_y - (self.y + (self.image.get_height() / 2))
+
+        if height != 0:
+            angle = math.atan2(width, height)
+            angle = math.degrees(angle)
+            angle += 180
+
+            # print(f'width: {width} \t height: {height} \t angle: {angle}')
+            # print(f'mouse x: {mouse_x} \t mouse y: {mouse_y}')
+            # print(f'tank x: {self.x} \t tank y: {self.y}')
+            # print(self.image.get_width())
+            
+            # Rotate turret
+            rotatedTurret = pygame.transform.rotate(self.turret, angle)
+
+            # Adjust the position of the rotated turret
+            turret_rect = rotatedTurret.get_rect()
+            center_x = self.x + (self.image.get_width() / 2)
+            center_y = self.y + (self.image.get_height() / 2)
+
+            # Calculate the new top left position based on the anchor point
+            turret_rect.center = (center_x, center_y)
+
+            return rotatedTurret, turret_rect
+        
+        else:
+            return self.turret, self.turret.get_rect()
---
+++ main.py
@@ -0,0 +1,68 @@
+import pygame
+from settings import Settings
+from blueTank import BlueTank
+
+class TanksGame:
+    def __init__(self):
+        pygame.init()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(self.settings.screenSize)
+        pygame.display.set_caption("Tanks")
+        self.background = pygame.image.load('mapImages/woodbackground.png')
+
+        self.player = BlueTank()
+
+        self.gameRunning = True
+
+    def runGame(self):
+        while self.gameRunning:
+            pygame.time.delay(10)
+
+            keys = pygame.key.get_pressed()
+
+            ## Movement
+            if keys[pygame.K_w] and keys[pygame.K_d]:
+                self.player.image = self.player.sprites[1]
+                self.player.move(1.41, -1.41)
+            elif keys[pygame.K_d] and keys[pygame.K_s]:
+                self.player.image = self.player.sprites[3]
+                self.player.move(1.41, 1.41)
+            elif keys[pygame.K_s] and keys[pygame.K_a]:
+                self.player.image = self.player.sprites[1]
+                self.player.move(-1.41, 1.41)
+            elif keys[pygame.K_a] and keys[pygame.K_w]:
+                self.player.image = self.player.sprites[3]
+                self.player.move(-1.41, -1.41)
+            elif keys[pygame.K_w]:
+                self.player.image = self.player.sprites[0]
+                self.player.move(0, -2)
+            elif keys[pygame.K_d]:
+                self.player.image = self.player.sprites[2]
+                self.player.move(2, 0)
+            elif keys[pygame.K_s]:
+                self.player.image = self.player.sprites[0]
+                self.player.move(0, 2)
+            elif keys[pygame.K_a]:
+                self.player.image = self.player.sprites[2]
+                self.player.move(-2, 0)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.gameRunning = False
+
+            self.displayScreen()
+        
+        pygame.quit()
+
+    def displayScreen(self):
+        self.screen.blit(self.background, (0, 0))
+        self.screen.blit(self.player.image, (self.player.x, self.player.y))
+        turret_img, turret_rect = self.player.getTurret()
+        self.screen.blit(turret_img, turret_rect.topleft)
+        # self.screen.blit(self.player.getTurret(), (self.player.turretX, self.player.turretY))
+        pygame.display.update()
+
+
+if __name__ == '__main__':
+    game = TanksGame()
+    game.runGame()
